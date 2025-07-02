c1fc599b30294cf90c6b0c01d5957c2045c43f56
david <david.watine@gmail.com>
2025-07-02 19:49:01 +0200

    tracé des ligne de metro

--- back.py
+++ back.py
@@ -9,6 +9,8 @@ from flask_cors import CORS
 from flask import render_template
 from data.MetroGraph import MetroGraph
 from graph.dijkstra import dijkstra, reconstruire_chemin
+import networkx as nx
+
 
 path = os.path.join(os.path.dirname(__file__), "data/metro_graph.pkl")
 with open(path, "rb") as f:
@@ -133,6 +135,58 @@ def get_shortest_path():
         "total_duration": total_duration,
         "steps": steps
     })
+
+
+
+@app.route("/api/lines")
+def get_lines():
+    from collections import defaultdict
+
+    # Dictionnaire {ligne: set de segments réellement consécutifs}
+    segments_par_ligne = defaultdict(set)
+    seen = set()
+
+    for stop1, voisins in metroGraph.graph.items():
+        for stop2, info in voisins.items():
+            # Éviter les doublons (A,B) == (B,A)
+            key = tuple(sorted([stop1.id, stop2.id]))
+            if key in seen:
+                continue
+            seen.add(key)
+
+            if (stop1.name == "Michel-Ange - Molitor" and stop2.name == "Porte d'Auteuil") or \
+                        (stop2.name == "Michel-Ange - Molitor" and stop1.name == "Porte d'Auteuil"):
+                            continue  # on saute cette liaison erronée
+
+            for route in info["routes"]:
+                coord1 = (round(stop1.lat, 6), round(stop1.lon, 6))
+                coord2 = (round(stop2.lat, 6), round(stop2.lon, 6))
+                segment = tuple(sorted([coord1, coord2]))  # pour éviter duplication inversée
+                segments_par_ligne[route].add(segment)
+
+    # Construction du GeoJSON par segment
+    lignes_geojson = []
+    for route, segments in segments_par_ligne.items():
+        for coord1, coord2 in segments:
+            lignes_geojson.append({
+                "name": f"Ligne {route}",
+                "color": get_line_color(route),
+                "coordinates": [list(coord1), list(coord2)]
+            })
+
+    return jsonify(lignes_geojson)
+
+
+
+def get_line_color(name):
+    palette = {
+        "1": "#FFCD00", "2": "#003CA6", "3": "#837902",
+        "4": "#CF009E", "5": "#FF7E2E", "6": "#6ECEB2",
+        "7": "#F5A9B8", "8": "#E19BDF", "9": "#B6BD00",
+        "10": "#C9910D", "11": "#704B1C", "12": "#007852",
+        "13": "#6EC4E8", "14": "#62259D"
+    }
+    return palette.get(name, "#999999")
 
 
 CORS(app)
